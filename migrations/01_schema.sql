60d0f05d89775aa82cd37af462286ea216465038
Spencer Lewis <spence.914@gmail.com>
2024-01-15 16:34:32 -0800

    fix spelling error in inital table creation query

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -18,10 +18,10 @@ CREATE TABLE properties (
   description TEXT,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
   cover_photo_url VARCHAR(255) NOT NULL,
-  cost_per_night INTEGER NOT NULL DEFAULT 0,
-  parking_spaces INTEGER NOT NULL DEFAULT 0,
-  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
-  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
+  cost_per_night INTEGER  NOT NULL DEFAULT 0,
+  parking_spaces INTEGER  NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
 
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
@@ -41,7 +41,7 @@ CREATE TABLE reservations (
 );
 
 CREATE TABLE property_reviews (
-  id SERIAL PRIMARY KEY NOT NUll,
+  id SERIAL PRIMARY KEY NOT NULL,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
