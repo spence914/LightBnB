05c504dbf7144de21d4789121e8e02e1535002b7
Spencer Lewis <spence.914@gmail.com>
2024-01-20 16:03:45 -0800

    fix bug where logged in users properties wouldn't display due to syntax error in getAllProperties function

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -105,7 +105,7 @@ const getAllProperties = function (options, limit = 10) {
 
   if (options.owner_id) {
     queryParams.push(`${options.owner_id}`);
-    queryString += ` AND owner_id LIKE $${queryParams.length}`;
+    queryString += ` AND owner_id = $${queryParams.length}`;
   }
 
   if (options.minimum_price_per_night) {
